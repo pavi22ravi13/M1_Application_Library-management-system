2113b90646ec8f8e493449784c2a6d7e0a4cca14
pavi22ravi13 <paviravi.1319@gmail.com>
2021-11-23 22:11:28 +0530

    Create library.h

---
+++ 3_Implementation/inc/library.h
@@ -0,0 +1,23 @@
+/**
+ * @file library.h
+ * @author Pavithra Ravi
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#ifndef LIBRARY_H_
+#define LIBRARY_H_
+
+struct library
+{
+char bk_name[30];
+char author[30];
+int pages;
+float price;
+};
+#endif
